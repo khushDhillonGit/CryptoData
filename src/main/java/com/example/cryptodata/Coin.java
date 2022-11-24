f907c9149a80fc1fa92ca75461111275ff3ec7ef
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-11-24 02:14:24 -0500

    made new coin class with all relevant attributes to get from API

---
+++ src/main/java/com/example/cryptodata/Coin.java
@@ -0,0 +1,39 @@
+package com.example.cryptodata;
+
+public class Coin {
+    private String symbol;
+    private String name;
+    private String iconUrl;
+    private String marketCap;
+    private String price;
+    private int rank;
+    private String change;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public String getMarketCap() {
+        return marketCap;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getChange() {
+        return change;
+    }
+}
