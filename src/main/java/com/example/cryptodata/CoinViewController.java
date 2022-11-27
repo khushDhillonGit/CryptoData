2e2a8692081c900c9e83f7e4870c9d0d6311c4f5
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-11-27 01:58:12 -0500

    added an error label to output the error messages

--- src/main/java/com/example/cryptodata/CoinViewController.java
+++ src/main/java/com/example/cryptodata/CoinViewController.java
@@ -5,11 +5,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+import javafx.scene.paint.Color;
 
 
 import java.io.IOException;
@@ -30,12 +31,16 @@ public class CoinViewController implements Initializable {
     @FXML
     private ImageView imageView;
 
+    @FXML
+    private Label errorLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         orderByCombo.getItems().addAll("price", "marketCap", "24hVolume", "change", "listedAt");
         orderByCombo.getSelectionModel().selectFirst();
         imageView.setImage(new Image(Main.class.getResourceAsStream("images/default-coin.jpg")));
-
+        errorLabel.setText("Crypto is Love!");
+        errorLabel.setTextFill(Color.color(1,0,0));
 
         dataList.getSelectionModel().selectedItemProperty().addListener((obs,old,coinSelected)->{
 
@@ -55,8 +60,15 @@ public class CoinViewController implements Initializable {
     void getCoins(ActionEvent event) throws IOException, InterruptedException {
         dataList.getItems().clear();
         String searchText = searchField.getText();
-        APIResponse apiResponse = APIUtility.getCoinsFromAPI(searchText,orderByCombo.getValue());
-        dataList.getItems().addAll(apiResponse.getData().getCoins());
+        if(!searchText.isBlank()) {
+            errorLabel.setText("Crypto is Love!");
+            APIResponse apiResponse = APIUtility.getCoinsFromAPI(searchText, orderByCombo.getValue());
+            dataList.getItems().addAll(apiResponse.getData().getCoins());
+
+        }else{
+            errorLabel.setText("Please enter a search value");
+        }
     }
+
 
 }
