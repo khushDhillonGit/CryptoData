39e0b9080b69173e0078c4dfefd2482632c87d5f
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-11-27 00:02:18 -0500

    added methods in CoinViewController to get coins from api and populate the list

--- src/main/java/com/example/cryptodata/CoinViewController.java
+++ src/main/java/com/example/cryptodata/CoinViewController.java
@@ -1,19 +1,42 @@
 package com.example.cryptodata;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class CoinViewController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CoinViewController implements Initializable {
 
     @FXML
-    private ListView<?> dataList;
+    private ListView<Coin> dataList;
 
     @FXML
-    private ChoiceBox<?> orderByCombo;
+    private ChoiceBox<String> orderByCombo;
 
     @FXML
     private TextField searchField;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        orderByCombo.getItems().addAll("price", "marketCap", "24hVolume", "change", "listedAt");
+        orderByCombo.getSelectionModel().selectFirst();
+
+    }
+
+    @FXML
+    void getCoins(ActionEvent event) throws IOException, InterruptedException {
+        dataList.getItems().clear();
+        String searchText = searchField.getText();
+        APIResponse apiResponse = APIUtility.getCoinsFromAPI(searchText,orderByCombo.getValue());
+        dataList.getItems().addAll(apiResponse.getData().getCoins());
+
+    }
 
 }
